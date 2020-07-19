8c0c5048583f626febe4c2df2e6a56bcd3f990ad
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-07-19 16:47:06 -0400

    overlay

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ window.onload = function () {
   fotos.addEventListener(
     "click",function(e) {
         if(e.target.tagName === "IMG"){
-            console.log("IMG")
+            // crea un div para el overlay
+            var fondoNegro = document.createElement("div");
+                fondoNegro.id ="overlay";
+                // se agraga al docuemnto
+                document.body.appendChild(fondoNegro);
+                // se indica posicion y background
+                fondoNegro.style.position="absolute";
+                fondoNegro.style.top=0;
+                fondoNegro.style.backgroundColor= "rgba(0,0,0,0.5)";
+                fondoNegro.style.cursor="pointer";
+
+                // se le asignan e ancho y largo de la ventana
+                fondoNegro.style.width = window.innerWidth+"px";
+                fondoNegro.style.height = window.innerHeight + "px";
+                // posicion con offset Y e X
+                fondoNegro.style.top = window.pageYOffset + "px";
+                fondoNegro.style.left = window.pageXOffset + "px";
         }else{
             console.log("No IMG")
         }
