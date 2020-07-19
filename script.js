0922f2233beb11ee5dcaadb71082fb7256775a98
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-07-19 17:38:19 -0400

    popUp an delete

--- script.js
+++ script.js
@@ -21,9 +21,40 @@ window.onload = function () {
                 // posicion con offset Y e X
                 fondoNegro.style.top = window.pageYOffset + "px";
                 fondoNegro.style.left = window.pageXOffset + "px";
+
+                //selecion imagen xl 
+                var mini = e.target.src;
+                var archivo = mini.substr(mini.length-10);
+                var imagenlx="./img/"+archivo
+
+                // crea tag img y carga la imagen grande
+                var imagen = document.createElement("img");
+                imagen.id = "image";
+                imagen.src=imagenlx;
+                imagen.style.display="block";
+                imagen.style.position = "absolute";
+                // prevencion en caso de no carga
+                imagen.addEventListener("load", function () {
+                    centrar(this);
+                    fondoNegro.appendChild(imagen);
+                },false);
+                imagen.addEventListener("click", function () {
+                    if(fondoNegro){
+                        fondoNegro.parentNode.removeChild(fondoNegro);
+                    }                    
+                },false)
+            
         }else{
             console.log("No IMG")
         }
     },
     false);
+    function centrar(imagen) {
+        var xx= (window.innerWidth-imagen.width)/2;
+        var yy= (window.innerHeight-imagen.height)/2;
+        imagen.style.top = yy+"px";
+        imagen.style.left = xx+"px";
+        return imagen;
+        
+    }
 };
